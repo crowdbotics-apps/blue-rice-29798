10a0aafb9ceb34c18ff8199043f84a0488ab8653
Crowdbotics <team@crowdbotics.com>
2021-08-13 23:23:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "blue_rice_29798Identifier",
+  url: "https://blue_rice_29798Identifier.botics.co",
+  api: "https://blue_rice_29798Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
